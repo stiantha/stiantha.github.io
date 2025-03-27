be1408718b8adfca4d21160c038d215413780d37
stian <stian1301@gmail.com>
2025-03-27 20:47:36 +0100

    Update GitHub Pages deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://stiantha.github.io',
   base: '/',
+  output: 'static',
+  trailingSlash: 'always',
   outDir: './dist',
   build: {
     assets: 'assets'
